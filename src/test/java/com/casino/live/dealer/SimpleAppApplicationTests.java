bed197195ed17baa8d1766e67c38f5b48b1c9d7d
mohamed chiheb ben jemaa <mc.benjemaa@gmail.com>
2019-03-19 22:38:02 +0100

    change unit test

--- src/test/java/com/casino/live/dealer/SimpleAppApplicationTests.java
+++ src/test/java/com/casino/live/dealer/SimpleAppApplicationTests.java
@@ -1,9 +1,12 @@
 package com.casino.live.dealer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,12 +16,15 @@ import com.medmedchiheb.simpleapp.SimpleAppApplication;
 @SpringBootTest
 public class SimpleAppApplicationTests {
 
+	Logger LOGGER = LoggerFactory.getLogger(SimpleAppApplicationTests.class);
+	
 	@Test
 	public void contextLoads() {
 	}
 	
 	@Test
 	public void testController() {
+		LOGGER.info(" >>> unit-test <<<");
 		SimpleAppApplication app = new SimpleAppApplication();
 		
 		assertThat(app.start()).isNotNull();
