f63a535c6e87137bba3d6300e47eec1cd7f83e8b
mohamed chiheb ben jemaa <mc.benjemaa@gmail.com>
2019-03-19 22:23:15 +0100

    change app

--- src/main/java/com/medmedchiheb/simpleapp/SimpleAppApplication.java
+++ src/main/java/com/medmedchiheb/simpleapp/SimpleAppApplication.java
@@ -1,4 +1,4 @@
-package com.casino.live.dealer;
+package com.medmedchiheb.simpleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/casino/live/dealer/SimpleAppApplicationTests.java
+++ src/test/java/com/casino/live/dealer/SimpleAppApplicationTests.java
@@ -1,9 +1,13 @@
 package com.casino.live.dealer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.medmedchiheb.simpleapp.SimpleAppApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,13 @@ public class SimpleAppApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testController() {
+		SimpleAppApplication app = new SimpleAppApplication();
+		
+		assertThat(app.start()).isNotNull();
 	}
 
 }
